819173352a64b7e0850884072b98001be7637a4f
EL2020 <el2020202020@gmail.com>
2021-03-05 16:05:33 -0500

    initial commit

---
+++ prestart.js
@@ -0,0 +1,93 @@
+//modifies the in-game parameter HUD
+sc.ParamHudGui.inject({ 
+    init(){
+        this.parent();
+        this.hp = new sc.ParamHudGui.Param("maxhp", "hp", 66, 99999, 0);
+        this.hp.setAlign(ig.GUI_ALIGN.X_LEFT, ig.GUI_ALIGN.Y_TOP);
+        this.hp.setPos(52, 0);
+        this.addChildGui(this.hp);
+        this.atk = new sc.ParamHudGui.Param("atk", "attack", 58, 9999, 1);
+        this.atk.setAlign(ig.GUI_ALIGN.X_LEFT, ig.GUI_ALIGN.Y_TOP);
+        this.atk.setPos(104, 0);
+        this.addChildGui(this.atk);
+        this.def = new sc.ParamHudGui.Param("def", "defense", 58, 9999, 2);
+        this.def.setAlign(ig.GUI_ALIGN.X_LEFT, ig.GUI_ALIGN.Y_TOP);
+        this.def.setPos(148,0);
+        this.addChildGui(this.def);
+        this.foc = new sc.ParamHudGui.Param("foc", "focus", 58, 9999, 3);
+        this.foc.setAlign(ig.GUI_ALIGN.X_LEFT, ig.GUI_ALIGN.Y_TOP);
+        this.foc.setPos(192, 0);
+        this.addChildGui(this.foc);
+    },
+})
+
+//modifies the actual hud of the GUI.
+sc.HpHudGui.inject({
+    init(){
+        this.parent();
+        this.setSize(68, 16);
+        this.removeChildGui(this.hpNumber); //removes the original HP number
+        
+        this.hpNumber = new sc.NumberGui(99999, {
+            signed: true,
+            transitionTime: 0.5
+        });
+        this.hpNumber.setPos(6, 1);
+        this.addChildGui(this.hpNumber);
+    }
+})
+
+
+/*
+ *  note to future self: this "works" but there's two of them... 
+ *  figure out a way to get rid of the original one!
+ *  i'm probably just doing it wrong, and that's okay! :)
+ */
+
+//uncaps on the equipment menu
+sc.EquipStatusContainer.inject({
+    init(){
+        this.parent();
+
+        let a = new sc.MenuPanel(sc.MenuPanelType.TOP_RIGHT_EDGE);
+        this.removeChildGui(a)
+
+        a.setSize(169, 121);
+        a.setPivot(0, 0);
+
+        this.addChildGui(a);
+
+        let b = 5, c = sc.model.player.equipParams;
+        this.baseParams.hp = this._createStatusDisplay(0, b, "maxhp", 0, 0, false, 99999, c.hp, void 0, a);
+        b = b + 14;
+        this.baseParams.atk = this._createStatusDisplay(0, b, "atk", 0, 1, false, 9999, c.attack, void 0, a);
+        b = b + 14;
+        this.baseParams.def = this._createStatusDisplay(0, b, "def", 0, 2, false, 9999, c.defense, void 0, a);
+        b = b + 14;
+        this.baseParams.foc = this._createStatusDisplay(0, b, "foc", 0, 3, false, 9999, c.focus, void 0, a);
+        b = b + 16;
+        this.baseParams.fire = this._createStatusDisplay(0, b, "res", 1, 4, true, 999, c.elemFactor[0], void 0, a);
+        b = b + 14;
+        this.baseParams.cold = this._createStatusDisplay(0, b, "res", 2, 5, true, 999, c.elemFactor[1], void 0, a);
+        b = b + 14;
+        this.baseParams.shock = this._createStatusDisplay(0, b, "res", 3, 6, true, 999, c.elemFactor[2], void 0, a);
+        this.baseParams.wave = this._createStatusDisplay(0, b + 14, "res", 4, 7, true, 999, c.elemFactor[3], void 0, a);
+    },
+})
+
+//uncaps status screen parameters, not the actual status screen
+sc.StatusViewMainParameters.inject({
+    init(){
+        this.parent();
+        var a = 97;
+        this.baseParams.hp = this.createStatusDisplay(0, a, "maxhp", 0, 0, false, 99999);
+        a = a + 14;
+        this.baseParams.atk = this.createStatusDisplay(0, a, "atk", 0, 1, false, 9999); //hey!!!!
+        a = a + 14;
+        this.baseParams.def = this.createStatusDisplay(0, a, "def", 0, 2, false, 9999);
+        a = a + 14;
+        this.baseParams.foc = this.createStatusDisplay(0, a, "foc", 0, 3, false, 9999);
+    }
+})
+
+//uncaps status screen
